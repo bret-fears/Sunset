6cb07dbbe9ea9a639a533e4ab7044a7c05cf67e8
bret-fears <bret.fears@willowtreeapps.com>
2016-12-16 08:25:18 -0500

    Chapter 30 without challenges

---
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -0,0 +1,85 @@
+package com.bignerdranch.android.sunset;
+
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+
+/**
+ * Created by bretfears on 12/15/16.
+ */
+
+public class SunsetFragment extends Fragment {
+
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
+
+    private AnimatorSet mAnimatorSet;
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
+        return view;
+    }
+
+    private void startAnimation() {
+        float sunYstart = mSunView.getTop();
+        float sunYend = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYstart, sunYend)
+                .setDuration(3000);
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        ObjectAnimator nightSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
+                .setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        mAnimatorSet = new AnimatorSet();
+        mAnimatorSet
+                .play(heightAnimator)
+                .with(sunsetSkyAnimator)
+                .before(nightSkyAnimator);
+        mAnimatorSet.start();
+    }
+}
