6ace4eb862a8f25830e00612a30c71f31fd1dc78
bret-fears <bret.fears@willowtreeapps.com>
2016-12-16 18:01:00 -0500

    Chapter-30 Challenges
    Add a sun rise animation to "reverse" the sunset
    Add infinite pulsating animation to the sun drawable

--- app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.sunset;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
@@ -11,12 +12,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 /**
  * Created by bretfears on 12/15/16.
  */
 
 public class SunsetFragment extends Fragment {
+
+    private static long DURATION = 3000L;
 
     private View mSceneView;
     private View mSunView;
@@ -25,8 +30,13 @@ public class SunsetFragment extends Fragment {
     private int mBlueSkyColor;
     private int mSunsetSkyColor;
     private int mNightSkyColor;
+    private SkyType mCurrentSkyType;
 
-    private AnimatorSet mAnimatorSet;
+    private AnimatorSet mCurrentAnimation;
+
+    private enum SkyType {
+        DAY, NIGHT
+    }
 
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
@@ -41,45 +51,104 @@ public class SunsetFragment extends Fragment {
         mSunView = view.findViewById(R.id.sun);
         mSkyView = view.findViewById(R.id.sky);
 
+        mCurrentAnimation = new AnimatorSet();
+
         Resources resources = getResources();
         mBlueSkyColor = resources.getColor(R.color.blue_sky);
         mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
         mNightSkyColor = resources.getColor(R.color.night_sky);
 
+        Animation pulse = AnimationUtils.loadAnimation(getContext(), R.anim.pulse);
+        mSunView.startAnimation(pulse);
+
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startAnimation();
+                runAnimation();
             }
         });
 
         return view;
     }
 
-    private void startAnimation() {
-        float sunYstart = mSunView.getTop();
-        float sunYend = mSkyView.getHeight();
+    private void runAnimation() {
+        float sunsetYstart = mSunView.getTop();
+        float sunriseYstart = mSkyView.getHeight();
+        float sunYend = mSkyView.getHeight() + 60; // pulsating offset
+        ObjectAnimator sunHeightAnimator;
+        ObjectAnimator sunSkyAnimator;
+        ObjectAnimator nightSkyAnimator;
 
-        ObjectAnimator heightAnimator = ObjectAnimator
-                .ofFloat(mSunView, "y", sunYstart, sunYend)
-                .setDuration(3000);
-        heightAnimator.setInterpolator(new AccelerateInterpolator());
+        if (mCurrentAnimation.isRunning()) {
+            return;
+        }
 
-        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
-                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
-                .setDuration(3000);
-        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+        mCurrentAnimation.end();
 
-        ObjectAnimator nightSkyAnimator = ObjectAnimator
-                .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
-                .setDuration(1500);
-        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+        if (mCurrentSkyType == null || mCurrentSkyType == SkyType.DAY) {
+            sunHeightAnimator = ObjectAnimator
+                    .ofFloat(mSunView, "y", sunsetYstart, sunYend)
+                    .setDuration(3000);
+            sunHeightAnimator.setInterpolator(new AccelerateInterpolator());
 
-        mAnimatorSet = new AnimatorSet();
-        mAnimatorSet
-                .play(heightAnimator)
-                .with(sunsetSkyAnimator)
+            sunSkyAnimator = ObjectAnimator
+                    .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                    .setDuration(3000);
+            sunSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+            nightSkyAnimator = ObjectAnimator
+                    .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
+                    .setDuration(1500);
+            nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+        } else {
+            sunHeightAnimator = ObjectAnimator
+                    .ofFloat(mSunView, "y", sunriseYstart + 60, mSunView.getTop())
+                    .setDuration(3000);
+            sunHeightAnimator.setInterpolator(new AccelerateInterpolator());
+
+            sunSkyAnimator = ObjectAnimator
+                    .ofInt(mSkyView, "backgroundColor", mNightSkyColor, mSunsetSkyColor)
+                    .setDuration(1000);
+            sunSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+            nightSkyAnimator = ObjectAnimator
+                    .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mBlueSkyColor)
+                    .setDuration(500);
+            nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+        }
+
+        mCurrentAnimation = new AnimatorSet();
+        mCurrentAnimation
+                .play(sunHeightAnimator)
+                .with(sunSkyAnimator)
                 .before(nightSkyAnimator);
-        mAnimatorSet.start();
+        mCurrentAnimation.start();
+
+        mCurrentAnimation.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (mCurrentSkyType == null || mCurrentSkyType == SkyType.DAY) {
+                    mCurrentSkyType = SkyType.NIGHT;
+                } else {
+                    mCurrentSkyType = SkyType.DAY;
+                }
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
